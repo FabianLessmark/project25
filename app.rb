2998b5e4da8650720da46311ff79dab7f44eb8e6
FabianLessmark <fabian.lessmark@elev.ga.ntig.se>
2025-02-25 10:36:42 +0100

    1

--- app.rb
+++ app.rb
@@ -4,3 +4,14 @@ require 'sqlite3'
 require 'sinatra/reloader'
 require 'bcrypt'
 
+get('/')  do
+  slim(:start)
+end 
+
+get('/games') do
+  slim(:games)
+end
+
+get('/account') do
+  slim(:account)
+end
