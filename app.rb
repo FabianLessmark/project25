6e46b5637019fdadb3d4b608bb16168cde14dca8
FabianLessmark <fabian.lessmark@elev.ga.ntig.se>
2025-04-08 11:41:26 +0200

    5

--- app.rb
+++ app.rb
@@ -29,7 +29,7 @@ post('/login') do
     session[:user_id] = user["id"]
     session[:access] = user["access"]
     session[:username] = user["username"]
-    session[:saldo] = user["saldo"]  # Här hämtar vi saldot och sparar i sessionen
+    session[:saldo] = user["saldo"]  
     redirect '/'
   else
     @error = "Wrong username or password, if first time: create account"
@@ -53,7 +53,7 @@ post('/register') do
 
   existing_user = DB.execute("SELECT * FROM user WHERE username = ?", username).first
   if existing_user
-    @error = "Användarnamnet är redan taget"
+    @error = "Username already taken"
     slim(:register)
   else
     DB.execute("INSERT INTO user (username, password, access, saldo) VALUES (?, ?, ?, ?)", [username, password_digest, "user", 100])
@@ -102,9 +102,49 @@ post('/shuffle_teams') do
   redirect '/games'
 end
 
+
 get('/bets/:game_id') do
   @game = DB.execute("SELECT games.id, teams1.name AS team1_name, teams2.name AS team2_name FROM games JOIN teams AS teams1 ON games.team_id1 = teams1.id JOIN teams AS teams2 ON games.team_id2 = teams2.id WHERE games.id = ?", params[:game_id]).first
-  slim(:bets)
+  
+  # Om spelet inte hittas, kan du omdirigera eller visa ett felmeddelande
+  if @game.nil?
+    redirect('/games')  # Eller annan passande sida
+  else
+    slim(:bets)
+  end
+end
+
+
+post('/place_bet') do
+  game_id = params[:game_id]
+  bet_amount = params[:bet_amount].to_i
+  bet_outcome = params[:bet]
+
+  if bet_amount < 0 || bet_amount > 20
+    @error = "Bet amount must be between 0 and 20."
+    slim(:bets)
+  else
+    user_id = session[:user_id]
+    user_balance = DB.execute("SELECT saldo FROM user WHERE id = ?", user_id).first["saldo"]
+
+    if user_balance < bet_amount
+      @error = "Exceeding your balance. You have #{user_balance} in your account."
+      slim(:bets)  # Visa felmeddelande och återgå till bettsidan
+    else
+      # Dra av bettbeloppet från saldot
+      DB.execute("UPDATE user SET saldo = saldo - ? WHERE id = ?", [bet_amount, user_id])
+
+      # Logik för att registrera bettet
+      DB.execute("INSERT INTO bets (user_id, game_id, bet_amount, bet_outcome) VALUES (?, ?, ?, ?)", 
+                 [user_id, game_id, bet_amount, bet_outcome])
+
+      # Uppdatera sessionen med det nya saldot
+      new_balance = user_balance - bet_amount
+      session[:saldo] = new_balance
+
+      redirect '/games'  # Eller någon annan sida där du vill visa resultatet
+    end
+  end
 end
 
 get('/admin/users') do
