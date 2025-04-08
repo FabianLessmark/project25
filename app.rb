d94d4a428c61a2e955f7138660a09271889a1b6c
FabianLessmark <fabian.lessmark@elev.ga.ntig.se>
2025-04-08 10:17:09 +0200

    4

--- app.rb
+++ app.rb
@@ -3,9 +3,63 @@ require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
 require 'bcrypt'
+require 'date'
+enable :sessions
+
 
 DB = SQLite3::Database.new "db/project2025.db"
 DB.results_as_hash = true  
+
+before do
+  pass if ["/login", "/register"].include?(request.path_info)
+  redirect('/login') unless session[:user_id]
+end
+
+get('/login') do
+  slim(:login)
+end
+
+post('/login') do
+  username = params[:username]
+  password = params[:password]
+
+  user = DB.execute("SELECT * FROM user WHERE username = ?", username).first
+
+  if user && BCrypt::Password.new(user["password"]) == password
+    session[:user_id] = user["id"]
+    session[:access] = user["access"]
+    session[:username] = user["username"]
+    session[:saldo] = user["saldo"]  # Här hämtar vi saldot och sparar i sessionen
+    redirect '/'
+  else
+    @error = "Wrong username or password, if first time: create account"
+    slim(:login)
+  end
+end
+
+get('/logout') do
+  session.clear
+  redirect '/login'
+end
+
+get('/register') do
+  slim(:register)
+end
+
+post('/register') do
+  username = params[:username]
+  password = params[:password]
+  password_digest = BCrypt::Password.create(password)
+
+  existing_user = DB.execute("SELECT * FROM user WHERE username = ?", username).first
+  if existing_user
+    @error = "Användarnamnet är redan taget"
+    slim(:register)
+  else
+    DB.execute("INSERT INTO user (username, password, access, saldo) VALUES (?, ?, ?, ?)", [username, password_digest, "user", 100])
+    redirect '/login'
+  end
+end
 
 def generate_games
   team_ids = DB.execute("SELECT id FROM teams").flatten.shuffle
@@ -51,4 +105,33 @@ end
 get('/bets/:game_id') do
   @game = DB.execute("SELECT games.id, teams1.name AS team1_name, teams2.name AS team2_name FROM games JOIN teams AS teams1 ON games.team_id1 = teams1.id JOIN teams AS teams2 ON games.team_id2 = teams2.id WHERE games.id = ?", params[:game_id]).first
   slim(:bets)
+end
+
+get('/admin/users') do
+  redirect('/') unless session[:access] == "admin"
+  @users = DB.execute("SELECT * FROM user")
+  slim(:admin_users)
+end
+
+post('/admin/users/delete/:id') do
+  redirect('/') unless session[:access] == "admin"
+  DB.execute("DELETE FROM user WHERE id = ?", params[:id])
+  redirect('/admin/users')
+end
+
+post('/add_funds') do
+  redirect('/login') unless session[:user_id]
+
+  user_id = session[:user_id]
+  DB.execute("UPDATE user SET saldo = saldo + 100 WHERE id = ?", user_id)
+
+  saldo = DB.execute("SELECT saldo FROM user WHERE id = ?", user_id).first["saldo"]
+  session[:saldo] = saldo
+
+  redirect back
+end
+
+get('/logout') do
+  session.clear
+  redirect '/login'
 end
